94625ea5bd0e0971bcc7fd36a8bed244cf2829fd
Emilio Scott <milii76@outlook.com>
2026-09-23 16:53:00 -0700

    fix(scripts): fix premature exit from a non-zero code in network config
    in `systemd-networkd-config.sh`, code would prematurely exit if one of
    the commands ran was inactive. This would prevent the other check from
    running. Putting the commands in explicit condition statements
    and manually echoing whether one of the
    services were active or inactive prevents prematurely exiting the code
    with a non-zero status.

--- scripts/check-active-networkmanagers.sh
+++ scripts/check-active-networkmanagers.sh
@@ -4,11 +4,19 @@
 # checks to see if systemd-networkd and NetworkManager are active
 set -euo pipefail
 
-echo 'checking NetworkManager...'
-systemctl is-active NetworkManager
-echo
+# having explicit if else branches prevents the code exiting prematurely due to a 
+# non-exit 0 from one of the commands. Perserves `set -euo pipefail` cleanly
+# while still maintaing the same logic
+if systemctl is-active --quiet systemd-networkd; then
+	echo 'systemd-networkd: ACTIVE'
+else
+	echo 'systemd-networkd: INACTIVE'
+fi
 
-echo 'checking systemd-networkd...'
-systemctl is-active systemd-networkd
-exit 0 
+if systemctl is-active --quiet NetworkManager; then 
+	echo 'NetworkManager: ACTIVE'
+else
+	echo 'NetworkManager: INACTIVE'
+fi
 
+exit 0
